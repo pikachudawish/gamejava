29517ba8e9aac957c27b313098e019888d0d5cfb
pikachudawish <ruben.costa.820@gmail.com>
2026-05-23 23:36:36 +0100

    further reorganization

---
+++ server-db/src/main/java/com/github/pikachudawish/serverDB.java
@@ -0,0 +1,9 @@
+package com.github.pikachudawish;
+
+public class serverDB {
+
+    public static void main(String[] args) {
+        int port = 6969;
+
+    }
+}
---
+++ server-game/src/main/java/com/github/pikachudawish/server.java
@@ -0,0 +1,15 @@
+package com.github.pikachudawish;
+
+public class server {
+    private int port;
+
+    public server(int port) {
+        this.port = port;
+    }
+
+
+    public static void main(String[] args) {
+        int port = 6767;
+
+    }
+}
